45e4ba648ceb3a27b7d67e1840ec4171c8b3bd1f
jajalejo21 <alejopalomino58@gmail.com>
2026-09-13 13:42:01 -0500

    feat: agregar rutas de Usuario y Producto y conectarlas a Express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,21 @@
+// src/app.js
 const express = require('express');
 const cors = require('cors');
+const usuarioRoutes = require('../routes/usuarioRoutes');
+const productoRoutes = require('../routes/productoRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Ruta de prueba
 app.get('/api/health', (req, res) => {
-  res.json({ mensaje: 'El servidor está funcionando' });
+  res.json({ mensaje: 'El servidor está funcionando 🚀' });
 });
+
+// Rutas del CRUD
+app.use('/api/usuarios', usuarioRoutes);
+app.use('/api/productos', productoRoutes);
 
 module.exports = app;
